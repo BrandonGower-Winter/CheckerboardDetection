b41e5ff2d9251846f8d71f143efac28618c48dcf
Brandon Gower-Winter <brandongowerwinter@gmail.com>
2024-09-06 16:43:55 +0200

    Stuff

---
+++ example_job.sh
@@ -0,0 +1,24 @@
+# Job Name
+#$ -N UM_Test
+# Notifications
+#$ -M b.gower-winter@uu.nl
+# When notified (b : begin, e : end, s : error)
+#$ -m bes
+# Execute from the current directory, and pass environment:
+#$ -cwd # use current directory
+#$ -V  # pass my complete environment to the job
+# define output file name
+#$ -o ./output.o
+# define error file name
+#$ -e ./error_output.o
+#cd <PATH TO YOUR PYTHON SCRIPT> #
+
+echo 'Start of Job:'
+# Set up the environment
+source ./venv/bin/activate
+# Install all needed modules:
+# Examples:
+# python -m pip install requirements.txt
+
+python3 -m python ./src/main.py -n 50 -f 1.0 --tau 1000 --config ./configs/self_defeating/default.json -o 0001.json --debug
+echo 'Finished computation.'
